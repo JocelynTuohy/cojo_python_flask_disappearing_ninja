785d753d3a5a661ba54b6ac4cb4b37af4adf28d9
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-06-08 17:57:34 -0700

    completed disappearing ninja assignment

--- server.py
+++ server.py
@@ -10,5 +10,11 @@ def index():
 def allNinjas():
     return render_template('allNinjas.html')
 
+@app.route('/ninja/<color>')
+def thisNinja(color):
+    return render_template('thisninja.html', color=color)
+
+
+
 app.run(debug=True)
 # Nothing after the debug line will be read.
